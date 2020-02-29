e7d1342799937305ff9507613806535d5435c91f
Anand Gupta <ganand1@contractor.jcp.com>
2020-02-29 21:57:42 +0530

    Added carousal component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Carousal from  './Components/Carousal/Carousal';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header" >
+				<Carousal />
+			</header>      
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Carousal/Carousal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Carousal.css';
+
+function App() {
+    return (
+        <div className="carousal">
+            Carousal Component Loaded...
+        </div>
+        );
+}
+
+export default App;
